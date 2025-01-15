a8a9acf9df33c0582645e4650a4ecdeef9339324
Artenlaclase <artenlaclase@gmail.com>
2025-01-14 22:50:12 -0300

    About

---
+++ src/components/About.tsx
@@ -0,0 +1,19 @@
+export default function About() {
+    return (
+      <section
+        id="sobre-nosotros"
+        className="py-16 px-4 bg-gray-100 text-gray-800"
+      >
+        <div className="max-w-4xl mx-auto text-center">
+          <h2 className="text-3xl font-bold mb-4">Acerca de Nosotros</h2>
+          <p className="text-lg leading-7">
+            En Oceánica Web, somos apasionados por crear páginas y aplicaciones
+            web que fusionan creatividad y tecnología. Nuestra misión es ofrecer
+            soluciones digitales que reflejen la belleza y la esencia del océano.
+            {/* Contenido restante */}
+          </p>
+        </div>
+      </section>
+    );
+  }
+  
